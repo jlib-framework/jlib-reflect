b624239cb7a24659b3534fdefb561cf57b3a5057
Igor Akkerman <igor.akkerman@jlib.org>
2015-02-24 10:03:19 +0100

    Reflector moved to own jlib module

---
+++ src/main/java/org/jlib/reflect/TypedClassReflector.java
@@ -0,0 +1,38 @@
+/*
+ * jlib - Open Source Java Library
+ *
+ *     www.jlib.org
+ *
+ *
+ *     Copyright 2005-2015 Igor Akkerman
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+package org.jlib.reflect;
+
+import org.jlib.core.classinstance.ClassInstanceException;
+import org.jlib.core.classinstance.WrongTypedInstanceException;
+
+public interface TypedClassReflector<Value> {
+
+    Class<Value> get()
+    throws ClassInstanceException;
+
+    TypedClassReflector<Value> assertSubtypeOf(Class<?> expectedSuperType)
+    throws WrongTypedInstanceException;
+
+    MethodOverloadReflector<Value> constructor();
+
+    UntypedMethodReflector useStaticMethod(String methodName);
+}
---
+++ src/test/java/org/jlib/reflect/ReflectorTest.java
@@ -0,0 +1,49 @@
+/*
+ * jlib - Open Source Java Library
+ *
+ *     www.jlib.org
+ *
+ *
+ *     Copyright 2005-2015 Igor Akkerman
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+package org.jlib.reflect;
+
+import org.assertj.core.api.Assertions;
+import static org.jlib.reflect.Reflectors.useClassNamed;
+import static org.jlib.reflect.SuperTypeValidator.instanceOf;
+import org.junit.Test;
+
+public class ReflectorTest {
+
+    @Test
+    @SuppressWarnings("UnnecessaryBoxing")
+    public void staticRun()
+    throws Exception {
+        final Number value = useClassNamed("java.lang.Integer") /*
+          */.assertType(Number.class)
+            .assertSubtypeOf(Integer.class)
+            .useStaticMethod("valueOf")
+            .withReturnType(Number.class)
+            .withArgumentTypes(int.class)
+            .invoke(42)
+            .assertReturned(instanceOf(Integer.class))
+            .assertReturned(Integer.valueOf(42))
+            .get();
+
+        Assertions.assertThat(value).isEqualTo(Integer.valueOf(42));
+    }
+}
+
